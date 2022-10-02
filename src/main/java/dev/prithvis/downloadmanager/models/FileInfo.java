7ec8c7b58d668841abb83838dfb0b88e82d2c740
prithvi <prithvisinghtewatia@gmail.com>
2022-10-02 10:26:03 +0530

    Modified File info to suit new UI changes

--- src/main/java/dev/prithvis/downloadmanager/models/FileInfo.java
+++ src/main/java/dev/prithvis/downloadmanager/models/FileInfo.java
@@ -1,18 +1,72 @@
 package dev.prithvis.downloadmanager.models;
 
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
-public class FileInfo {
-    private SimpleIntegerProperty index;
-    private SimpleStringProperty name;
-    private SimpleStringProperty url;
-    private SimpleStringProperty status;
 
-    public FileInfo(int index,String name,String url,String status){
-        this.index.set(index);
+public class FileInfo {
+    private final SimpleStringProperty location=new SimpleStringProperty();
+    private final SimpleStringProperty name=new SimpleStringProperty();
+    private final SimpleStringProperty url=new SimpleStringProperty();
+    private final SimpleStringProperty status=new SimpleStringProperty();
+    private final SimpleStringProperty addedOn=new SimpleStringProperty();
+
+    public String getLocation() {
+        return location.get();
+    }
+
+    public SimpleStringProperty locationProperty() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location.set(location);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name.set(name);
+    }
+
+    public String getUrl() {
+        return url.get();
+    }
+
+    public SimpleStringProperty urlProperty() {
+        return url;
+    }
+
+    public void setUrl(String url) {
         this.url.set(url);
+    }
+
+    public String getStatus() {
+        return status.get();
+    }
+
+    public SimpleStringProperty statusProperty() {
+        return status;
+    }
+
+    public void setStatus(String status) {
         this.status.set(status);
+    }
+
+    public String getAddedOn() {
+        return addedOn.get();
+    }
+
+    public SimpleStringProperty addedOnProperty() {
+        return addedOn;
+    }
+
+    public void setAddedOn(String addedOn) {
+        this.addedOn.set(addedOn);
     }
 }
